6486ccc496a68c09ada7b539a6816ab143d3c25b
root <e.chulak@hotmail.com>
2017-01-02 20:57:48 +0000

    added removeAttributes and removeAllAttributes to IDController

--- contracts/IDController.sol
+++ contracts/IDController.sol
@@ -4,9 +4,14 @@ import "ID.sol";
 
 contract IDController is owned {
     ID id;
-    string name;
     function IDController(ID _id){
         id = _id;    
-        name = "hello";
+    }
+    function removeAttribute(bytes32 key) onlyowner returns (string){
+        return id.removeAttribute(key);
+    }
+    
+    function removeAllAttributes() onlyowner{
+       id.removeAllAttributes();
     }
 }
