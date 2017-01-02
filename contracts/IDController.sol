bfc3556168e4724f58c09b507e0b25c976e2dddd
root <olibd@outlook.com>
2017-01-02 20:57:09 +0000

    added addAttribute, getAttributeLocation and deleteID functions

--- contracts/IDController.sol
+++ contracts/IDController.sol
@@ -9,4 +9,17 @@ contract IDController is owned {
         id = _id;    
         name = "hello";
     }
+    
+    function addAttribute(bytes32 key, string attrLocation) onlyowner returns (string){
+        return id.addAttribute(key, attrLocation);
+    }
+    
+    function getAttributeLocation(bytes32 key) returns (string){
+        return id.getAttributeLocation(key);
+    }
+    
+    function deleteID() onlyowner{
+        id.kill();
+        if (msg.sender == owner) suicide(owner);
+    }
 }
