832155b3c35615c8a62ba0eb9036ac31a1500ad1
root <e.chulak@hotmail.com>
2017-01-02 20:45:34 +0000

    created IDController and updated factory to create the IDController

--- contracts/Factory.sol
+++ contracts/Factory.sol
@@ -1,11 +1,20 @@
 pragma solidity ^0.4.2;
 import "std.sol";
 import "ID.sol";
+import "IDController.sol";
+
 contract Factory is owned {
-    function createID() returns (address){
+    function createID() returns (IDController){
         ID newID = new ID();
         newID.changeOwner(msg.sender);
-        return newID;
+        return  createIDController(newID, msg.sender);
+    }
+    
+    function createIDController (ID id, address sender) private returns (IDController){
+        IDController idController = new IDController(id);
+        id.changeOwner(idController);
+        idController.changeOwner(sender);
+        return idController;
     }
 }
 
