9ae79bc67816f6fdf36f203dd7c0a733d55faded
root <olibd@outlook.com>
2017-01-30 23:04:51 +0000

    stated writing the watchdog test

--- test/watchdogTest.js
+++ test/watchdogTest.js
@@ -1,5 +1,17 @@
 contract('Watchdog', function(accounts) {
-  it("should put 10000 MetaCoin in the first account", function(done) {
+  
+  it("test propose() function", function(done) {
+      var watchdog = Watchdog.deployed();
+      var idCtrl = IDController.deployed();
       
+      //First add a second owner
+      watchdog.addMultisigOwner(accounts[1])
+      .then(function(tx){
+        //Then issue a proposal
+        
+      });
+      
+      done();
   });
+  
 });
