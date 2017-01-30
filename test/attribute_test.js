a51a54d376ba0078861026b524c555b177108e4d
root <olibd@outlook.com>
2017-01-30 23:03:04 +0000

    added catch statement if the test fails

--- test/attribute_test.js
+++ test/attribute_test.js
@@ -4,12 +4,23 @@ contract('Attribute', function(accounts) {
     var attribute = Attribute.deployed();
 
     //set the location
-    return attribute.setLocation(location).then(function(tx){
+    return attribute.setLocation(location).then(function(tx) {
+      
       //get the location
-      return attribute.getLocation.call().then(function(setLoc){
+      return attribute.getLocation.call().then(function(setLoc) {
+        console.log(setLoc);
         assert.equal(setLoc, location);
         done();
+      }).catch(function(e) {
+        console.log(e);
+        assert.isTrue(false);
+        done();
       });
+      
+    }).catch(function(e) {
+      console.log(e);
+      assert.isTrue(false);
+      done();
     });
   });
 });
