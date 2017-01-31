b26f80ae7497046a8c9e52580cf37117dbc2bf9c
root <olibd@outlook.com>
2017-01-31 21:18:58 +0000

    added watchdogproxy contract to the test. this contract will behave as a second actor that will confirm the proposal of the first actor

--- src/tests/Watchdog_test.sol
+++ src/tests/Watchdog_test.sol
@@ -2,11 +2,18 @@ pragma solidity ^0.4.7;
 import "dapple/test.sol"; // virtual "dapple" package imported when `dapple test` is run
 import "Watchdog.sol";
 import "Factory.sol";
+import "IDController.sol";
 
 contract WatchdogTest is Test {
+    Factory factory;
+    IDController idCtrl;
+    Watchdog watchdog;
     
     function setUp() {
-
+        factory = new Factory();
+        idCtrl = factory.createID();
+        watchdog = new Watchdog([], 1);
+        idCtrl.setWatchDogs(watchdog);
     }
     
     function proposeTest() {
@@ -23,5 +30,22 @@ contract WatchdogTest is Test {
     
     function clearPendingTest() {
         
+    }
+}
+
+//This proxy contract will act as a second actor calling the watchdog contract
+contract WatchDogTestProxy{
+    Watchdog watchdog;
+    
+    function WatchDogTestProxy(Watchdog _watchdog){
+        watchdog = _watchdog;
+    }
+    
+    function propose(address _destination, uint _value, bytes _data) returns (bytes32 _r){
+        return watchdog.propose(_destination, _value, _data);
+    }
+    
+    function confirm(bytes32 _h) returns (bool){
+        return watchdog.confirm(_h);
     }
 }
