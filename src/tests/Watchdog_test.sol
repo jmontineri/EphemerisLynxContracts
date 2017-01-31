b540cbf8e5038dbe8377d66b2011d039ad034ced
root <olibd@outlook.com>
2017-01-31 02:52:05 +0000

    created the whatdog test and wrote the skeleton and moved the string_test to the tests folder

---
+++ src/tests/Watchdog_test.sol
@@ -0,0 +1,27 @@
+pragma solidity ^0.4.7;
+import "dapple/test.sol"; // virtual "dapple" package imported when `dapple test` is run
+import "Watchdog.sol";
+import "Factory.sol";
+
+contract WatchdogTest is Test {
+    
+    function setUp() {
+
+    }
+    
+    function proposeTest() {
+        
+    }
+    
+    function confirmTest() {
+        
+    }
+    
+    function cancelTest() {
+        
+    }
+    
+    function clearPendingTest() {
+        
+    }
+}
