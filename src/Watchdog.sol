9cfcf8c7b555f2813fb609676986af4a2ed46a7e
root <olibd@outlook.com>
2017-02-07 00:06:32 +0000

    completed the watchdog contract test, the watchdog contract was modified and bug were fixed the have the tests pass

--- src/Watchdog.sol
+++ src/Watchdog.sol
@@ -1,23 +1,28 @@
 pragma solidity ^0.4.7;
 import "MultiOwned.sol";
-import "IMultisig.sol";
-contract Watchdog is IMultisig, MultiOwned {
+import "IDController.sol";
+contract Watchdog is MultiOwned {
     
     // pending transaction we have at present.
-    Proposal proposal;
+    Proposal public proposal;
     
     // TYPES
 
     // Proposal structure to remember details of transaction lest it need be saved for a later call.
     struct Proposal {
-        address destination;
-        uint value;
-        bytes data;
+        IDController callDestination;
+        //uint value;
+        //bytes data;
+        address newOwner;
         address initiator;
         bytes32 hash; //hash communicated to the other watchdog. 
                     //Ensures that if the proposal changes by the time the 
                     //watchdog votes. He won't be able to cast a vote
     }
+    
+    //EVENTS
+    event NewProposal(bytes32 operation, address initiator, address to, address newOwner);
+    event ProposalConfirmed(bytes32 operation, address initiator, address lastSignatory, address to, address newOwner);
 
     // METHODS
 
@@ -30,16 +35,35 @@ contract Watchdog is IMultisig, MultiOwned {
     // We provide a hash on return to allow the sender to provide
     // shortcuts for the other confirmations (allowing them to avoid replicating the _to, _value
     // and _data arguments).
-    function propose(address _destination, uint _value, bytes _data) external onlyoneowner returns (bytes32 _r) {
+    /*function propose(IDController _callDestination, uint _value, bytes _data) external onlyoneowner returns (bytes32 _r) {
         
         // determine our operation hash.
         _r = sha3(msg.data, block.number);
         
         //if there is no current proposal
         if (proposal.hash == 0) {
-            proposal = Proposal(_destination, _value, _data, msg.sender, _r);
-            ConfirmationNeeded(_r, msg.sender, _value, _destination, _data);
+            proposal = Proposal(_callDestination, _value, _data, msg.sender, _r);
+            ConfirmationNeeded(_r, msg.sender, _value, _callDestination, _data);
         }
+    }*/
+    
+    function proposeMigration(IDController _callDestination, address newOwner) onlyoneowner returns (bytes32){
+    
+        //if there is no current proposal
+        if (proposal.hash == 0) {
+            // determine our operation hash.
+            bytes32 _r = sha3(msg.data, block.number);
+            proposal = Proposal(_callDestination, newOwner, msg.sender, _r);
+            NewProposal(_r, msg.sender, _callDestination, newOwner);
+            confirm(proposal.hash);
+            return proposal.hash;
+        } else
+            return 0;
+    }
+    
+    
+    function proposeDeletion(IDController _callDestination) onlyoneowner returns (bytes32 _r){
+        return proposeMigration(_callDestination, 0);
     }
     
     // confirm a transaction through just the hash
@@ -48,20 +72,19 @@ contract Watchdog is IMultisig, MultiOwned {
         if(proposal.hash != _h)
             throw;
         
-        if (proposal.destination != 0) {
+        if (address(proposal.callDestination) != 0) {
             
-            // If a contract tries to CALL or CREATE a contract with either
-    	    // (i) insufficient balance, or (ii) stack depth already at maximum (1024),
-    	    // the sub-execution and transfer do not occur at all, no gas gets consumed, and 0 is added to the stack.
-        	// see: https://github.com/ethereum/wiki/wiki/Subtleties#exceptional-conditions
-            if(!proposal.destination.call.value(proposal.value)(proposal.data))
-                throw;
+            //if we have a deletion proposal
+            if(proposal.newOwner == 0)
+                proposal.callDestination.deleteID(); //delete the ID
+            else
+                proposal.callDestination.changeOwner(proposal.newOwner); //if not migrate the id
                 
-            MultiTransact(msg.sender, proposal.hash, proposal.value, proposal.destination, proposal.data);
+            ProposalConfirmed(proposal.hash, proposal.initiator, msg.sender, proposal.callDestination, proposal.newOwner);
             delete proposal;
             return true;
         }else
-            return false;
+            throw;
     }
     
     // cancel a transaction through just the hash
@@ -76,6 +99,17 @@ contract Watchdog is IMultisig, MultiOwned {
             return true;
         }else
             return false;
+    }
+    
+    function getProposal() constant returns (IDController, address, address, bytes32){
+                IDController callDestination;
+        //uint value;
+        //bytes data;
+        address newOwner;
+        address initiator;
+        bytes32 hash;
+        
+        return (proposal.callDestination, proposal.newOwner, proposal.initiator, proposal.hash);
     }
     
     // INTERNAL METHODS
--- src/tests/Watchdog_test.sol
+++ src/tests/Watchdog_test.sol
@@ -3,49 +3,128 @@ import "dapple/test.sol"; // virtual "dapple" package imported when `dapple test
 import "Watchdog.sol";
 import "Factory.sol";
 import "IDController.sol";
+import "ID.sol";
 
 contract WatchdogTest is Test {
     Factory factory;
     IDController idCtrl;
     Watchdog watchdog;
+    //proxy contract to send call as a second actor. The first actor is the
+    //current contract
+    WatchDogTestProxy actor2;
+    Tester proxyTester;
     
     function setUp() {
         factory = new Factory();
         idCtrl = factory.createID();
-        watchdog = new Watchdog([], 1);
+        watchdog = new Watchdog(new address[](0), 2);
+        
+        if(watchdog.m_numOwners() != 1)
+            throw;
+        if(watchdog.m_required() != 2)
+            throw;
+        
+        proxyTester = new Tester();
+        proxyTester._target(watchdog);
+        
         idCtrl.setWatchDogs(watchdog);
-    }
-    
-    function proposeTest() {
         
+        actor2 = new WatchDogTestProxy(watchdog);
+        //add the second actor as an authorised watchdog 
+        watchdog.addMultisigOwner(actor2);
     }
     
-    function confirmTest() {
-        
+    function testProposeMigration() returns (bytes32 proposalHash){
+        //assert current owner is this contract
+        assertEq(idCtrl.getOwner(), this);
+        proposalHash = watchdog.proposeMigration(idCtrl, actor2);
+        // test if proposal was created
+        assertFalse(proposalHash == 0);
+        // test if we confirmed
+        assertTrue(watchdog.hasConfirmed(proposalHash, this));
+        //TODO: test events
     }
     
-    function cancelTest() {
-        
+    function testProposeDeletion() returns (bytes32 proposalHash){
+        proposalHash = watchdog.proposeDeletion(idCtrl);
+        // test if proposal was created
+        assertFalse(proposalHash == 0);
+        // test if we confirmed
+        assertTrue(watchdog.hasConfirmed(proposalHash, this));
+        //TODO: test events
+        //TODO: test an for the new owner with address 0
     }
     
-    function clearPendingTest() {
+    function testConfirmMigration() {
+        bytes32 proposalHash = testProposeMigration();
+        //make sure we can't confirm again
+        assertFalse(watchdog.confirm(proposalHash));
+        //make sure actor 2 hasnt confirm
+        assertFalse(watchdog.hasConfirmed(proposalHash, actor2));
+        //confirm, since have a m_required of 2 the operation should execute
+        assertTrue(actor2.confirm(proposalHash));
+        //the new owner should be actor2
+        assertEq(idCtrl.getOwner(), actor2);
+        //TODO: test events
+    }
+    
+    function testConfirmDeletion() {
+        bytes32 proposalHash = testProposeDeletion();
+        //make sure we can't confirm again
+        assertFalse(watchdog.confirm(proposalHash));
+        ID id = idCtrl.getID();
+        //make sure actor 2 hasnt confirm
+        assertFalse(watchdog.hasConfirmed(proposalHash, actor2));
+        //confirm, since have a m_required of 2 the operation should execute
+        assertTrue(actor2.confirm(proposalHash));
+        //TODO: test call on suicided contract
+    }
+    
+    function testCancel() {
+        bytes32 proposalHash = testProposeMigration();
+        //only the initiator can cancel
+        assertFalse(actor2.cancel(proposalHash));
+        assertTrue(watchdog.cancel(proposalHash));
+        //actor tries to confirm, should fail
+        assertFalse(actor2.confirm(proposalHash));
+        //make sure the proposal was deleted
+        var(callDestination, newOwner, initiator, hash) = watchdog.getProposal();
+        assertEq32(hash, 0);
+    }
+    
+    function testGetProposal() {
+        bytes32 proposalHash = testProposeMigration();
+        //make sure the current proposal is equal to the proposal we just made
+        var(callDestination, newOwner, initiator, hash) = watchdog.getProposal();
+        assertEq32(hash, proposalHash);
+        assertEq(newOwner, actor2);
+        assertEq(initiator, this);
+        assertEq(callDestination, idCtrl);
         
     }
 }
 
 //This proxy contract will act as a second actor calling the watchdog contract
-contract WatchDogTestProxy{
+contract WatchDogTestProxy {
     Watchdog watchdog;
     
-    function WatchDogTestProxy(Watchdog _watchdog){
+    function WatchDogTestProxy(Watchdog _watchdog) {
         watchdog = _watchdog;
     }
     
-    function propose(address _destination, uint _value, bytes _data) returns (bytes32 _r){
-        return watchdog.propose(_destination, _value, _data);
+    function proposeMigration(IDController _callDestination, address newOwner) returns (bytes32 _r){
+        return watchdog.proposeMigration(_callDestination, newOwner);
+    }
+    
+    function proposeDeletion(IDController _callDestination) returns (bytes32 _r){
+        return watchdog.proposeDeletion(_callDestination);
     }
     
     function confirm(bytes32 _h) returns (bool){
         return watchdog.confirm(_h);
+    }
+    
+    function cancel(bytes32 _h) returns (bool){
+        return watchdog.cancel(_h);
     }
 }
