789d1773564bd049a4520173b2e9c2ecd1dc06ca
Olivier Brochu Dufour <olibd@outlook.com>
2017-03-16 21:31:00 +0000

    added storage for the certificates in the form of a mapping. Added functions addCertificate and getCertificate.

--- src/Attribute.sol
+++ src/Attribute.sol
@@ -1,7 +1,10 @@
 pragma solidity ^0.4.7;
 import "Owned.sol";
 contract Attribute is Owned{
-    string private location;
+    string public location;
+    mapping (address => Certificate ) public certificates;
+    //Array of keys to iterate over the mapping
+    address[] public certificateKeys;
 
     function Attribute(string _location){
         setLocation(_location);
@@ -10,8 +13,21 @@ contract Attribute is Owned{
     function setLocation(string _location) onlyowner{
         location = _location;
     }
-
-    function getLocation() constant returns (string){
-        return location;
+    
+    function addCertificate(Certificate _cert) onlyowner{
+        //check the certificate is really for this id
+        if(_cert.owningAttribute != this)
+            throw;
+        //maker sure the the one adding is the issuer
+        if(_cert.owner != msg.sender)
+            throw;
+            
+        certificates[_cet.owner] = _cert;
+        certificateKeys.push(_cet.owner);
+        
+    }
+    
+    function getCertificate(address issuer) returns (Certificate){
+        return certificates[issuer];
     }
 }
