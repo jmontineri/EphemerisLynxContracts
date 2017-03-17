a6afe69d59ba5d33f64a89e8f241ace455ee2617
Olivier Brochu Dufour <olibd@outlook.com>
2017-03-17 01:09:01 +0000

    Added tests for the Attribute contract and the Certificate contract

---
+++ src/tests/Attribute_test.sol
@@ -0,0 +1,50 @@
+pragma solidity ^0.4.7;
+import "dapple/test.sol"; // virtual "dapple" package imported when `dapple test` is run
+import "Certificate.sol";
+
+contract AttributeTest is Test {
+    Certificate cert;
+    Attribute attr;
+    AttributeTestProxy attrProxy;
+    
+    function setUp(){
+        attrProxy = new AttributeTestProxy();
+        attr = new Attribute("hogwarts", "1234", attrProxy);
+        attrProxy.setAttr(attr);
+        cert = attrProxy.createCertificate();
+    }
+    
+    function testAddAndGetCertificate(){
+        
+        //add the certificate not as the owner of the attribute
+        attr.addCertificate(cert);
+        assertFalse(attr.getCertificate(this) == cert);
+        
+        //add the certificate as the owner of the attribute
+        attrProxy.addCertificate(cert);
+        assertEq(attr.getCertificate(attrProxy), cert);
+    }
+}
+
+//This proxy contract will act as a second actor calling the watchdog contract
+contract AttributeTestProxy{
+    Attribute public attr;
+    Certificate public cert;
+    
+    function createCertificate() returns (Certificate){
+        //The certificate needs to be owned by the message sender so that the 
+        //contract accepts it
+        if(address(attr) == 0)
+            throw;
+            
+        return cert = new Certificate("over the rainbow", "5678", attr);
+    }
+
+    function setAttr(Attribute _attr){
+        attr = _attr;
+    }
+    
+    function addCertificate(Certificate _cert){
+        attr.addCertificate(_cert);
+    }
+}
---
+++ src/tests/Certificate_test.sol
@@ -0,0 +1,41 @@
+pragma solidity ^0.4.7;
+import "dapple/test.sol"; // virtual "dapple" package imported when `dapple test` is run
+import "Attribute.sol";
+
+contract CertificateTest is Test {
+    CertificateTestProxy certProxy;
+    Attribute attr;
+    Certificate cert;
+    
+    function setUp() {
+        attr = new Attribute("neverland", "5678", address(this));
+        certProxy = new CertificateTestProxy(attr);
+        cert = certProxy.cert();
+    }
+    
+    function testRevoke(){
+        //test initial state
+        assertFalse(cert.revoked());
+        
+        //revoke directly from this contract should fail
+        cert.revoke();
+        assertFalse(cert.revoked());
+        
+        //revoking from the proxy should work
+        certProxy.revoke();
+        assertTrue(cert.revoked());
+    }
+}
+
+//This proxy contract will act as a second actor calling the watchdog contract
+contract CertificateTestProxy{
+    Certificate public cert;
+    
+    function CertificateTestProxy(Attribute attr){
+        cert = new Certificate("somewhere", "1234", attr);
+    }
+    
+    function revoke(){
+        cert.revoke();
+    }
+}
