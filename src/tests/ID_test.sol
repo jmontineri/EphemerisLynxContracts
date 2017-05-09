10115eff8dbb3f1c94e372203c01da9eeefd38a1
jmontineri <jmontineri@gmail.com>
2017-05-09 15:50:16 -0400

    Replaced string key with hash of string

--- src/tests/ID_test.sol
+++ src/tests/ID_test.sol
@@ -12,48 +12,50 @@ contract IDTest is Test{
     Attribute attr;
     Certificate cert;
     DummyOwner newOwner;
+    bytes32 key;
     
     function setUp() {
         id1 = new ID();
         attr = new Attribute("test attr", "5678", this);
         cert = new Certificate("test cert", "1234", attr);
         newOwner = new DummyOwner();
+        key = sha3("hello");
     }
     
     function testAddAndGetAttribute(){
         
         //Adding attribute to ID        
-        id1.addAttribute("hello", attr);
-        assertEq(id1.getAttribute("hello"), attr);
+        id1.addAttribute(key, attr);
+        assertEq(id1.getAttribute(key), attr);
         //Make sure ID is the owner of the attribute
-        assertEq(id1.getAttribute("hello").owner(), address(id1));
+        assertEq(id1.getAttribute(key).owner(), address(id1));
     }
 
     function testAddAndRemoveAttribute(){
         //Adding attribute to ID, again      
-        id1.addAttribute("hello", attr);
-        assertEq(id1.getAttribute("hello"), attr);
+        id1.addAttribute(key, attr);
+        assertEq(id1.getAttribute(key), attr);
         //Removing attribute from ID - should not be equal any more
-        id1.removeAttribute("hello");
-        assertFalse(id1.getAttribute("hello") == attr);
+        id1.removeAttribute(key);
+        assertFalse(id1.getAttribute(key) == attr);
     }
 
     function testAddCertificateByKey(){
         //Adding attribute and cert to attribute by key
-        id1.addAttribute("hello", attr);
-        id1.addCertificate("hello", cert);
+        id1.addAttribute(key, attr);
+        id1.addCertificate(key, cert);
         //Getting the certificate issued by this contract
-        Certificate testedCert = id1.getAttribute("hello")
+        Certificate testedCert = id1.getAttribute(key)
                                     .getCertificate(this);
         assertEq(testedCert, cert);
     }
 
     function testAddCertificateByAttribute(){
         //Adding attribute and cert to attribute by key
-        id1.addAttribute("hello", attr);
+        id1.addAttribute(key, attr);
         id1.addCertificate(attr, cert);
         //Getting the certificate issued by this contract
-        Certificate testedCert = id1.getAttribute("hello")
+        Certificate testedCert = id1.getAttribute(key)
                                     .getCertificate(this);
         assertEq(testedCert, cert);    
     }
@@ -65,7 +67,7 @@ contract IDTest is Test{
 
     function testCreateCertificate{
         //Creating attribute and cert for that attribute
-        id1.addAttribute("hello", attr);
+        id1.addAttribute(key, attr);
         Certificate newCert = id1.createCertificate("created certificate", "2323", attr);
         //Making sure the new cert belongs to the ID that created it
         assertEq(new.owner, id1);
